6e0e10c285158ea5a158ecd45ab6b2f5f57e5c9d
lujunji4113 <lujunji4113@gmail.com>
2024-02-16 17:47:38 +0800

    Change output file path

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "rollup";
 export default defineConfig({
   input: "./src/index.js",
   output: {
-    file: "./dist/cjs/index.js",
+    file: "./dist/index.js",
     format: "commonjs",
   },
 });
